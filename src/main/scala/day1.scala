70d25fe430e47704fdf69728ac8d369c38908083
Kristina Nylander <kristina.nylander@kvirkla.se>
2021-12-01 06:22:57 +0100

    It works

--- src/main/scala/day1.scala
+++ src/main/scala/day1.scala
@@ -1,12 +1,24 @@
 import scala.io.Source
 
-def solve(lines: List[String]) : String =
-  lines.foreach(println)
-  "foo"
+def solve(numbers: List[Int], prev: Int = Int.MaxValue, acc: Int = 0) : Int =
+  if numbers.isEmpty then
+    acc
+  else if numbers.head > prev then
+    solve(numbers.tail, numbers.head, acc+1)
+  else
+    solve(numbers.tail, numbers.head, acc)
+
+def consecutive(numbers: List[Int]): List[Int] =
+  if numbers.size < 3 then
+    List.empty
+  else
+    List(numbers.take(3).sum) ++ consecutive(numbers.drop(1))
 
 
 @main
 def main(): Unit =
-  val lines = Source.fromResource("day1.txt").getLines()
+  val lines = Source.fromResource("day1.txt").getLines().map(_.toInt).toList
 
-  println("Pt1: $solv")
+  println(solve(consecutive(List(199,200, 208, 210, 200, 207, 240, 269, 260, 263))))
+  println("Pt1: " + solve(lines))
+  println("Pt2: " + solve(consecutive(lines)))
