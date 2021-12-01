9e8650f5cb19671d6a7e298bd39ea6041acff138
Kristina Nylander <kristina.nylander@kvirkla.se>
2021-12-01 17:46:03 +0100

    A bit of cleanup

--- src/main/scala/day1.scala
+++ src/main/scala/day1.scala
@@ -1,24 +1,18 @@
 import scala.io.Source
 
 def solve(numbers: List[Int], prev: Int = Int.MaxValue, acc: Int = 0) : Int =
-  if numbers.isEmpty then
-    acc
-  else if numbers.head > prev then
-    solve(numbers.tail, numbers.head, acc+1)
-  else
-    solve(numbers.tail, numbers.head, acc)
+  numbers match {
+    case Nil => acc
+    case n1 :: tail if n1 > prev => solve(tail, n1, acc+1)
+    case _ => solve(numbers.tail, numbers.head, acc)
+  }
 
-def consecutive(numbers: List[Int]): List[Int] =
-  if numbers.size < 3 then
-    List.empty
-  else
-    List(numbers.take(3).sum) ++ consecutive(numbers.drop(1))
-
+def consecutiveSumsOfThree(numbers: List[Int]): List[Int] =
+  numbers.sliding(3).map(_.sum).toList
 
 @main
 def main(): Unit =
   val lines = Source.fromResource("day1.txt").getLines().map(_.toInt).toList
 
-  println(solve(consecutive(List(199,200, 208, 210, 200, 207, 240, 269, 260, 263))))
   println("Pt1: " + solve(lines))
-  println("Pt2: " + solve(consecutive(lines)))
+  println("Pt2: " + solve(consecutiveSumsOfThree(lines)))
