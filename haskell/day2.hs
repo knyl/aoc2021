4102a333d3a4335ca0b277e60db9aae0dcaad130
Kristina Nylander <kristina.nylander@kvirkla.se>
2021-12-06 19:17:04 +0100

    Cleanup

--- haskell/day2.hs
+++ haskell/day2.hs
@@ -6,30 +6,29 @@ import Data.List
 import Prelude
 
 data Instruction = Forward Int | Up Int | Down Int
-data Position = Position Int Int | AimPos Int Int Int
+data Position = SimplePosition Int Int | AimPosition Int Int Int
 
 main = do
   input <- readFile "resources/day2.txt"
   let instructions = map toInstruction (lines input)
-  let finalPosition1 = foldl applyPosition (Position 0 0) instructions
-  let finalPosition2 = foldl applyPosition (AimPos 0 0 0) instructions
+  let finalPosition1 = foldl applyPosition (SimplePosition 0 0) instructions
+  let finalPosition2 = foldl applyPosition (AimPosition 0 0 0) instructions
   print $ getResult finalPosition1
   print $ getResult finalPosition2
 
 toInstruction :: String -> Instruction
 toInstruction (stripPrefix "forward " -> Just number) = Forward (read number)
-toInstruction (stripPrefix "up " -> Just number) = Up (read number)
-toInstruction (stripPrefix "down " -> Just number) = Down (read number)
+toInstruction (stripPrefix "up "      -> Just number) = Up (read number)
+toInstruction (stripPrefix "down "    -> Just number) = Down (read number)
 
 applyPosition :: Position -> Instruction -> Position
-applyPosition (Position x depth) (Forward amount) = Position (x + amount) depth
-applyPosition (Position x depth) (Up amount) = Position x (depth - amount)
-applyPosition (Position x depth) (Down amount) = Position x (depth + amount)
-
-applyPosition (AimPos x depth aim) (Forward amount) = AimPos (x + amount) (depth + aim * amount) aim
-applyPosition (AimPos x depth aim) (Up amount) = AimPos x depth (aim - amount)
-applyPosition (AimPos x depth aim) (Down amount) = AimPos x depth (aim + amount)
+applyPosition (SimplePosition x depth)  (Forward amount) = SimplePosition (x + amount) depth
+applyPosition (SimplePosition x depth)  (Up amount)      = SimplePosition x (depth - amount)
+applyPosition (SimplePosition x depth)  (Down amount)    = SimplePosition x (depth + amount)
+applyPosition (AimPosition x depth aim) (Forward amount) = AimPosition (x + amount) (depth + aim * amount) aim
+applyPosition (AimPosition x depth aim) (Up amount)      = AimPosition x depth (aim - amount)
+applyPosition (AimPosition x depth aim) (Down amount)    = AimPosition x depth (aim + amount)
 
 getResult :: Position -> Int
-getResult (Position x depth) = x * depth
-getResult (AimPos x depth _) = x * depth
+getResult (SimplePosition x depth) = x * depth
+getResult (AimPosition x depth _)  = x * depth
