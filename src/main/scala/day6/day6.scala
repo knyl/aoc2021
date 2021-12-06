292be09c6c72040c1785573c00972846d93cceb7
Kristina Nylander <kristina.nylander@kvirkla.se>
2021-12-06 08:12:22 +0100

    fix

--- src/main/scala/day6/day6.scala
+++ src/main/scala/day6/day6.scala
@@ -5,7 +5,7 @@ import scala.io.Source
 
 type State = Map[Int, BigInt]
 
-def default = BigInt(0)
+val default = BigInt(0)
 
 def solve(initialState: State, iterations: Int): BigInt =
   val finalState = (0 until iterations).foldLeft(initialState)((state, _) => nextState(state))
@@ -27,9 +27,8 @@ def nextState(state: State): State =
 @main
 def main(): Unit =
   val numbers = Source.fromResource("day6.txt").getLines().flatMap(_.split(",")).map(_.toInt).toList
-  val initialState: State = numbers.groupBy(identity).map { (key, list) => (key, BigInt(list.size)) }
+  val example = "3,4,3,1,2"
+  val initialState: State = numbers.groupBy(identity).map((key, list) => (key, BigInt(list.size)))
 
   println("Pt1: " + solve(initialState, 80))
   println("Pt2: " + solve(initialState, 256))
-
-def example() = "3,4,3,1,2"
