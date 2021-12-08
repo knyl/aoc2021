09f8b4c0d18a0766800185a10ff536e47decd784
Kristina Nylander <kristina.nylander@kvirkla.se>
2021-12-08 17:14:51 +0100

    Cleanup, still kind of incomprehensible

--- scala/src/main/scala/day8/day8.scala
+++ scala/src/main/scala/day8/day8.scala
@@ -2,21 +2,19 @@ package day8
 
 import scala.io.Source
 
-case class Numbers(d1: List[String], d2: List[String])
+case class Entry(patterns: List[String], output: List[String])
 
-def solve1(lines: List[Numbers]): Int =
-  lines.map(_.d2).flatMap(_.filter(s => s.length == 2 || s.length == 3 || s.length == 4 || s.length == 7)).size
+def solve1(lines: List[Entry]): Int =
+  lines.map(_.output).flatMap(_.filter(s => s.length == 2 || s.length == 3 || s.length == 4 || s.length == 7)).size
 
-def solve2(lines: List[Numbers]): Int =
-  val mappings = lines.map(getMapping)
-  val finalNumbers: List[List[String]] = (lines.map(_.d2) zip mappings) map((outputs, mapping) => applyMapping(outputs, mapping))
-  finalNumbers.map(decode).sum
+def solve2(entries: List[Entry]): Int =
+  val finalNumbers = entries.map(entry => (entry.output, getTranslation(entry))).map(applyTranslation)
+  finalNumbers.map(decodeOutput).sum
 
-def decode(number: List[String]): Int =
-  number.map(decode).mkString.toInt
+def decodeOutput(number: List[String]): Int =
+  number.map(decodeDigit).mkString.toInt
 
-def decode(num: String): Char =
-  sortedNum(num) match {
+def decodeDigit(digit: String): Char = sortedNum(digit) match {
   case "abcefg" => '0'
   case "cf" => '1'
   case "acdeg" => '2'
@@ -27,24 +25,30 @@ def decode(num: String): Char =
   case "acf" => '7'
   case "abcdefg" => '8'
   case "abcdfg" => '9'
-  case _ => throw RuntimeException(s"$num can't be decoded")
+  case _ => throw RuntimeException(s"$digit can't be decoded")
 }
 
 def sortedNum(num: String): String =
   num.toList.sorted.mkString
 
-def getMapping(line: Numbers): Map[Char, Char] = // TODO
-  val input = line.d1
-  val char1 = line.d1.find(_.length == 2).orNull
-  val char7 = line.d1.find(_.length == 3).orNull
-  val char4 = line.d1.find(_.length == 4).orNull
-  val char8 = line.d1.find(_.length == 7).orNull
+def getTranslation(line: Entry): Map[Char, Char] = // TODO
+  val patterns = line.patterns
+
+  val char1 = findOfLength(patterns, 2)
+  val char7 = findOfLength(patterns, 3)
+  val char4 = findOfLength(patterns, 4)
+  val char8 = findOfLength(patterns, 7)
+
   val a = char7.filter(!char1.contains(_)).head
-  val (char9, g) = deduceChar(char4 + a, line.d1.filter(_.length == 6))
-  val (char3, d) = deduceChar(char7 + g, line.d1.filter(_.length == 5))
+
+  val (char9, g) = deduceChar(char4 + a, patterns.filter(_.length == 6))
+  val (char3, d) = deduceChar(char7 + g, patterns.filter(_.length == 5))
+
   val b = char4.filter(!(char1 + d).contains(_)).head
   val e = char8.filter(!char9.contains(_)).head
-  val (char2, c) = deduceChar(List(a, d, e, g).mkString, line.d1.filter(_.length == 5))
+
+  val (char2, c) = deduceChar(List(a, d, e, g).mkString, patterns.filter(_.length == 5))
+
   val f = char1.filter(_ != c).head
   Map(
     (a, 'a'),
@@ -56,18 +60,22 @@ def getMapping(line: Numbers): Map[Char, Char] = // TODO
     (g, 'g'),
   )
 
+def findOfLength(patterns: List[String], length: Int): String = {
+  patterns.find(_.length == length).orNull
+}
+
 def deduceChar(filter: String, chars: List[String]): (String, Char) =
   val num = chars.find(str => str.count(!filter.contains(_)) == 1).orNull
   (num, num.filter(!filter.contains(_)).head)
 
-def applyMapping(outputs: List[String], mapping: Map[Char, Char]): List[String] =
+def applyTranslation(outputs: List[String], mapping: Map[Char, Char]): List[String] =
   outputs.map(toNum(_, mapping))
 
 def toNum(str: String, mapping: Map[Char, Char]): String =
   str.flatMap(mapping.get).mkString
 
-def parseInput(input: String): Numbers = input match {
-  case s"$n1 |$n2" => Numbers(toNums(n1), toNums(n2))
+def parseInput(input: String): Entry = input match {
+  case s"$n1 |$n2" => Entry(toNums(n1), toNums(n2))
   case _ => throw RuntimeException(s"Input: $input not matching")
 }
 
