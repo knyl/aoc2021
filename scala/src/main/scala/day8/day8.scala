c175f6c341c75f7e840a99a6c17090e9c1c1eeff
Kristina Nylander <kristina.nylander@kvirkla.se>
2021-12-08 08:08:20 +0100

    Solutions day 8

---
+++ scala/src/main/scala/day8/day8.scala
@@ -0,0 +1,120 @@
+package day8
+
+import scala.io.Source
+
+case class Numbers(d1: List[String], d2: List[String])
+
+def solve1(lines: List[Numbers]): Int =
+  lines.map(_.d2).flatMap(_.filter(s => s.length == 2 || s.length == 3 || s.length == 4 || s.length == 7)).size
+
+def solve2(lines: List[Numbers]): Int =
+  val mappings = lines.map(getMapping)
+  val finalNumbers: List[List[String]] = (lines.map(_.d2) zip mappings) map((outputs, mapping) => applyMapping(outputs, mapping))
+  finalNumbers.map(decode).sum
+
+def decode(number: List[String]): Int =
+  number.map(decode).mkString.toInt
+
+def decode(num: String): Char =
+  val sn = sortedNum(num)
+  sn match {
+  case "abcefg" => '0'
+  case "cf" => '1'
+  case "acdeg" => '2'
+  case "acdfg" => '3'
+  case "bcdf" => '4'
+  case "abdfg" => '5'
+  case "abdefg" => '6'
+  case "acf" => '7'
+  case "abcdefg" => '8'
+  case "abcdfg" => '9'
+  case _ => throw RuntimeException(s"$num can't be decoded")
+}
+
+def sortedNum(num: String): String =
+  val numList = num.toList
+  val sortedNumList = numList.sorted
+  val string = sortedNumList.mkString
+  num.toList.sorted.mkString
+
+def getMapping(line: Numbers): Map[Char, Char] = // TODO
+  //println("get mapping for line " + line)
+  val input = line.d1
+  val char1 = line.d1.find(_.length == 2).orNull
+  val char7 = line.d1.find(_.length == 3).orNull
+  val char4 = line.d1.find(_.length == 4).orNull
+  val char8 = line.d1.find(_.length == 7).orNull
+  val a = char7.filter(!char1.contains(_)).head
+  //println(s"a: $a")
+  val (char9, g) = getG(char4 + a, line.d1.filter(_.length == 6))
+  //println(s"g: $g")
+  val (char3, d) = getD(char7 + g, line.d1.filter(_.length == 5))
+  //println(s"d: $d")
+  val b = char4.filter(!(char1 + d).contains(_)).head
+  //println(s"b: $b")
+  val e = char8.filter(!char9.contains(_)).head
+  //println(s"e: $e")
+  val (char2, c) = getC(List(a, d, e, g).mkString, line.d1.filter(_.length == 5))
+  //println(s"c: $c")
+  val f = char1.filter(_ != c).head
+  //println(s"f: $f")
+  Map(
+    (a, 'a'),
+    (b, 'b'),
+    (c, 'c'),
+    (d, 'd'),
+    (e, 'e'),
+    (f, 'f'),
+    (g, 'g'),
+  )
+
+def getG(char4a: String, chars: List[String]): (String, Char) =
+  val num9 = chars.find(str => str.count(!char4a.contains(_)) == 1).orNull
+  (num9, num9.filter(!char4a.contains(_)).head)
+
+def getD(char7g: String, chars: List[String]): (String, Char) =
+  val num3 = chars.find(str => str.count(!char7g.contains(_)) == 1).orNull
+  (num3, num3.filter(!char7g.contains(_)).head)
+
+def getC(adeg: String, chars: List[String]): (String, Char) =
+  val num2 = chars.find(str => str.count(!adeg.contains(_)) == 1).orNull
+  (num2, num2.filter(!adeg.contains(_)).head)
+
+def applyMapping(outputs: List[String], mapping: Map[Char, Char]): List[String] =
+  val foo = outputs.map(toNum(_, mapping))
+  foo
+
+def toNum(str: String, mapping: Map[Char, Char]): String =
+  str.flatMap(mapping.get).mkString
+
+def parseInput(input: String): Numbers = input match {
+  case s"$n1 |$n2" => Numbers(toNums(n1), toNums(n2))
+  case _ => throw RuntimeException(s"Input: $input not matching")
+}
+
+def toNums(string: String): List[String] =
+  string.stripTrailing.stripLeading.split(" ").map(sortedNum).toList
+
+@main
+def main(): Unit =
+  val input = Source.fromResource("day8.txt").getLines().filterNot(_.isBlank).map(parseInput).toList
+  //val input = example.split("\n").filterNot(_.isEmpty).map(parseInput).toList
+  //val input = List("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab |cdfeb fcadb cdfeb cdbaf").map(parseInput)
+  //val input = List("gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc |fgae cfgab fg bagce").map(parseInput)
+
+  println("Pt1: " + solve1(input))
+  println("Pt2: " + solve2(input))
+
+val example =
+  """
+    |be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb |fdgacbe cefdb cefbgd gcbe
+    |edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec |fcgedb cgb dgebacf gc
+    |fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef |cg cg fdcagb cbg
+    |fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega |efabcd cedba gadfec cb
+    |aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga |gecf egdcabf bgf bfgea
+    |fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf |gebdcfa ecba ca fadegcb
+    |dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf |cefg dcbef fcge gbcadfe
+    |bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd |ed bcgafe cdgba cbgef
+    |egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg |gbdfcae bgc cg cgb
+    |gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc |fgae cfgab fg bagce
+    |""".stripMargin
